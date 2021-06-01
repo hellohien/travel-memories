4c77ae9630e2efbdcd3b4b33675560fc4e40bb84
Hien Nguyen <hnguy179@gmail.com>
2021-06-01 22:28:00 +0000

    Cleaned up code. #3

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header>
-      <a href="#addEntry"><h1><i className="logo fa fa-map-marker" aria-hidden="true"></i>TM</h1></a>
-      <div>
+      <a href="#"><h1><i className="logo fa fa-map-marker" aria-hidden="true"></i>TM</h1></a>
+      <div className="nav">
         <a href="#addEntry">Add Entry</a>
         <a href="#myMemories">My Memories</a>
       </div>
